c0ebadd8146b61eec489c269676cbb54758932c0
Adam <adamb@channelgrabber.com>
2013-09-26 15:24:55 +0100

    added extra config install

--- recipes/module_igbinary.rb
+++ recipes/module_igbinary.rb
@@ -1,3 +1,13 @@
 php_pear "igbinary" do
   action :install
 end
+
+script "configure_igbinary" do
+  if {File.exists?('/etc/php5/conf.d/igbinary.ini')}
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    echo -e "session.serialize_handler=igbinary
+    igbinary.compact_strings=On" >> /etc/php5/conf.d/mongo.ini
+  EOH
+end
