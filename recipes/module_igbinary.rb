9c0b9c4187a50bc4a31e9e5d156696b89953fdd3
Adam <adamb@channelgrabber.com>
2013-09-26 16:03:14 +0100

    fixed strings

--- recipes/module_igbinary.rb
+++ recipes/module_igbinary.rb
@@ -1,4 +1,4 @@
 php_pear "igbinary" do
   action :install
-  directives("session.serialize_handler" => igbinary, "igbinary.compact_strings" => On)
+  directives('session.serialize_handler' => 'igbinary', 'igbinary.compact_strings' => 'On')
 end
