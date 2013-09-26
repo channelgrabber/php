91b4e5e8b40e7cf6f514718589c1faca5fe0d8e0
Adam <adamb@channelgrabber.com>
2013-09-26 09:17:06 +0100

    removed channel updates

--- recipes/module_pecl_http.rb
+++ recipes/module_pecl_http.rb
@@ -1,11 +1,3 @@
-php_pear_channel 'pear.php.net' do
-  action :update
-end
-php_pear_channel 'pecl.php.net' do
-  action :update
-end
-
-# Install pecl_http
 php_pear "pecl_http" do
   action :install
 end
