0d8ca42b8fbb1e0a922ec2c7e7b285e0fda65ca5
Adam <adamb@channelgrabber.com>
2013-09-26 15:56:36 +0100

    use directives

--- recipes/module_igbinary.rb
+++ recipes/module_igbinary.rb
@@ -1,13 +1,4 @@
 php_pear "igbinary" do
   action :install
-end
-
-script "configure_igbinary" do
-  only_if {File.exists?('/etc/php5/conf.d/igbinary.ini')}
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-echo -e "session.serialize_handler=igbinary
-igbinary.compact_strings=On" >> /etc/php5/conf.d/igbinary.ini
-  EOH
+  directives("session.serialize_handler" => "igbinary", "igbinary.compact_strings" => "On")
 end
