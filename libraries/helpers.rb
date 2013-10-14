1b2ddfe45e9dc421bbf5871c32536d869d8c87ef
Adam <adamb@channelgrabber.com>
2013-10-14 14:08:07 +0100

    added file exist check

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -23,6 +23,7 @@ def el5_range
 end
 
 def enable_pear_mod (module_name)
+  if (File.exists('/etc/php5/conf.d/#{module_name}.ini'))
   bash "enable_pear_mod" do
     user "root"
     cwd '/etc/php5/conf.d'
