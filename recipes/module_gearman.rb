77fb9b3cefb7b41f9349657d6bc0777a0a0732ba
Adam <adamb@channelgrabber.com>
2013-11-01 14:32:22 +0000

    CGIV-553 removed debug

--- recipes/module_gearman.rb
+++ recipes/module_gearman.rb
@@ -1,10 +1,6 @@
 pear_module = "gearman"
 
-Chef::Log.error "******************** inside module_gearman"
-
 include_recipe "gearman::server-source"
 include_recipe "gearman::php"
 
-Chef::Log.error "******************** about to call eneable pear mod"
 enable_pear_mod (pear_module)
-
