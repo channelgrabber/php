9f71ac2cbb39d55fc373cf0a85ea6f94fc5f7ed7
Adam <adamb@channelgrabber.com>
2013-11-21 13:58:15 +0000

    CGIV-479 tidy config

--- recipes/module_xdebug.rb
+++ recipes/module_xdebug.rb
@@ -4,12 +4,11 @@ bash "configure_xdebug" do
   user "root"
   cwd "/etc/php5/mods-available"
   code <<-EOH
-    echo '[xdebug]
-    xdebug.remote_enable=1
-    xdebug.remote_connect_back=1
-    xdebug.idekey=PHPSTORM
-    xdebug.profiler_enable=1
-    xdebug.profiler_enable_trigger=1' >> xdebug.ini
+echo 'xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.idekey=PHPSTORM
+xdebug.profiler_enable=1
+xdebug.profiler_enable_trigger=1' >> xdebug.ini
   EOH
   action :nothing
 end
