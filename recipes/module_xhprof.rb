58d985712b6e73fec75bb16a893d680e7432ee09
James Moorhouse <jamesm@channelgrabber.com>
2014-05-29 10:44:52 +0100

    CGIV-2289 make dir and finish reset of installation

--- recipes/module_xhprof.rb
+++ recipes/module_xhprof.rb
@@ -1,5 +1,20 @@
-git "/var/channelgrabber/xh_prof" do
+xhprof_dir = "/var/channelgrabber/xh_prof"
+
+FileUtils.mkdir_p(xhprof_dir) unless File.exists?(xhprof_dir)
+
+git xhprof_dir do
 	repository "git://github.com/facebook/xhprof.git"
 	revision "master"
 	action :sync
+end
+
+bash 'install xhprof extension' do
+	cwd xhprof_dir
+	code <<-EOH
+		phpize
+		./configure --with-php-config={pathtophp-config} 
+		make
+		make install
+		make test
+		EOH
 end
