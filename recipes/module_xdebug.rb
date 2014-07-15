aee688b7ef7e3a0011fbed136a39c45a168ff2bb
Adam Blanchard <adamgoodie@gmail.com>
2014-07-15 12:31:48 +0100

    CGIV-1535 install xdebug package with apt

--- recipes/module_xdebug.rb
+++ recipes/module_xdebug.rb
@@ -1,22 +1,22 @@
 module_name = "xdebug"
 pkg = "php5-xdebug"
 
-bash "configure_xdebug" do
-  user "root"
-  cwd "/etc/php5/mods-available"
-  code <<-EOH
-echo 'xdebug.remote_enable=1
-xdebug.remote_connect_back=1
-xdebug.idekey=PHPSTORM
-xdebug.profiler_enable=1
-xdebug.profiler_enable_trigger=1' >> xdebug.ini
-  EOH
-  action :nothing
-end
+# bash "configure_xdebug" do
+#   user "root"
+#   cwd "/etc/php5/mods-available"
+#   code <<-EOH
+# echo 'xdebug.remote_enable=1
+# xdebug.remote_connect_back=1
+# xdebug.idekey=PHPSTORM
+# xdebug.profiler_enable=1
+# xdebug.profiler_enable_trigger=1' >> xdebug.ini
+#   EOH
+#   action :nothing
+# end
 
-package pkg do
+apt pkg do
   action :install
-  notifies :run, "bash[configure_xdebug]"
+#   notifies :run, "bash[configure_xdebug]"
   if node.recipe?('php-fpm')
     notifies :restart, "service[php-fpm]", :delayed
   end
