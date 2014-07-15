0bc16dce85fc7b13f454d8ba2ae20f8f0511e014
Adam Blanchard <adamgoodie@gmail.com>
2014-07-15 14:00:35 +0100

    CGIV-1535 reintroduce xdebug config

--- recipes/module_xdebug.rb
+++ recipes/module_xdebug.rb
@@ -1,24 +1,24 @@
 module_name = "xdebug"
 pkg = "php5-xdebug"
 
-# bash "configure_xdebug" do
-#   user "root"
-#   cwd "/etc/php5/mods-available"
-#   code <<-EOH
-# echo 'xdebug.remote_enable=1
-# xdebug.remote_connect_back=1
-# xdebug.idekey=PHPSTORM
-# xdebug.profiler_enable=1
-# xdebug.profiler_enable_trigger=1' >> xdebug.ini
-#   EOH
-#   action :nothing
-# end
+bash "configure_xdebug" do
+  user "root"
+  cwd "/etc/php5/mods-available"
+  code <<-EOH
+echo 'xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.idekey=PHPSTORM
+xdebug.profiler_enable=1
+xdebug.profiler_enable_trigger=1' >> xdebug.ini
+  EOH
+  action :nothing
+end
 
-Chef::Log.error "********************* installing xdebug package"
+Chef::Log.error "********************* installing xdebug package with config"
 
 package pkg do
   action :install
-#   notifies :run, "bash[configure_xdebug]"
+  notifies :run, "bash[configure_xdebug]"
   if node.recipe?('php-fpm')
     notifies :restart, "service[php-fpm]", :delayed
   end
