ae61acb1bd189b4c72f320751cb1bcdd18d12194
dangerous <dangerousdan@channelgrabber.com>
2015-02-06 12:12:02 +0000

    CGIv-4761 update php

--- recipes/package.rb
+++ recipes/package.rb
@@ -17,6 +17,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+package "php" do
+  action :upgrade
+end
+
 node['php']['packages'].each do |pkg|
   package pkg do
     options "-y"
