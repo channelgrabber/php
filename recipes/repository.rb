03986166075f2a3e0640ae7737134884b3fc15c8
dangerous <dangerousdan@channelgrabber.com>
2015-02-06 10:37:31 +0000

    CGIv-4761 updated ldebug

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -2,8 +2,8 @@
 #Channel Grabber Recipe to install php packages from external repo
 #####
 
-Chef::Log.debug('  ****  php repository  ****');
-Chef::Log.debug("Error #{node['php']['repository']}")
+Chef::Log.info('  ****  php repository  ****');
+Chef::Log.info("Error #{node['php']['repository']}")
 
 if node['php']['repository']['uri'].to_s != '' 
   apt_repository "php5" do
